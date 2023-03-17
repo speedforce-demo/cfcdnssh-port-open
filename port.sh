8de5c9f28e24c8a724d642ea0fe54bb34ec3be6a
speedforce-demo <122682099+speedforce-demo@users.noreply.github.com>
2023-03-17 16:18:48 +0530

    Create port.sh

---
+++ port.sh
@@ -0,0 +1,17 @@
+sudo apt-get install firewalld -y
+sudo firewall-cmd --zone=public --permanent --add-port=22/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=80/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=443/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=109/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=143/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=2086/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=1194/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=2200/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=990/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=89/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=7100/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=7200/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=7300/tcp
+sudo firewall-cmd --reload
+(crontab -l && echo "#ELHX-UI") | crontab -
+(crontab -l && echo "@reboot systemctl start firewalld") | crontab -
